ee992fe29bd834ee9b82cc73b12ec2fc7197287b
Boboaca Costel <bobyca2003@yahoo.com>
2020-03-26 17:11:00 +0100

    Add files via upload

--- extensions/costycnc-gcode.py
+++ extensions/costycnc-gcode.py
@@ -2,21 +2,6 @@
 
 import inkex, cubicsuperpath, simplepath, cspsubdiv
 
-def rotate(input,d): 
-   # Slice string in two parts for left and right
-	inkex.debug("rotate")
-	inkex.debug(d)
-	inkex.debug("finish rotate")
-	Lfirst = input[0 : d] 
-	Lsecond = input[d :] 
-	Rfirst = input[0 : len(input)-d] 
-	Rsecond = input[len(input)-d : ] 
-	total = Lsecond + Lfirst  
-	#total = input
-	return total
-
-
-
 class MyEffect(inkex.Effect):
 	def __init__(self):
 		inkex.Effect.__init__(self)
@@ -24,7 +9,7 @@ class MyEffect(inkex.Effect):
 									 dest="setup", default="left", help=("Settings"))
 		self.OptionParser.add_option("-f", "--flatness",
 						action="store", type="int", 
-						dest="flat", default=1,
+						dest="flat", default=.1,
 						help="Minimum flatness of the subdivided curves")									
 			
 	def effect(self):
@@ -46,98 +31,61 @@ class MyEffect(inkex.Effect):
 				cspsubdiv.cspsubdiv(p, .1)   
 				#find low y value
 				
-				cp = []                				
+				cp = [] 
 				g=0
 				h=0
-				if setup=='left':
-					c=p[0][0][0][0]               
-					for csp in p[0]:                   
-						b = csp[0][0]
-						e = csp[0][1]
-						if (b < c):
-							c = b
-							f = e 
-							h = g
-						cp.append([[csp[0][0],csp[0][1]]])
-						g += 1  
-				if setup=='up':
-					f=p[0][0][0][1]               
-					for csp in p[0]:                   
-						b = csp[0][0]
-						e = csp[0][1]
-						if (e < f):
-							c = b
-							f = e 
-							h = g
-						cp.append([[csp[0][0],csp[0][1]]])
-						g += 1 
-				if setup=='down':
-					f=0 
-					e=0
-					for csp in p[0]: 
-						cp.append([[csp[0][0],csp[0][1]]])                    
-						if (e > f):
-							c = b
-							f = e 
-							h = g
-						b = csp[0][0]
-						e = csp[0][1] 
-						g += 1 
-				if setup=='right':
-					c=0 
-					b=0
-					for csp in p[0]: 
-						cp.append([[csp[0][0],csp[0][1]]])                    
-						if (b > c):
-							c = b
-							f = e 
-							h = g
-						b = csp[0][0]
-						e = csp[0][1] 
-						g += 1 
-				if setup=='manual':
-					h=int(self.options.flat)
-					for csp in p[0]: 
-						cp.append([[csp[0][0],csp[0][1]]]) 
-				'''
-				b=0
-				e=0
-				g=0
-				c=p[0][0][0][0]
-				f=0
-				h=0
-				cp = []               
+				#-------------------------------------------
+				c=p[0][0][0][0]               
 				for csp in p[0]:                   
 					b = csp[0][0]
-					e = csp[0][1]
 					if (b < c):
 						c = b
+				f=p[0][0][0][1]               
+				for csp in p[0]:   
+					e = csp[0][1]
+					if (e < f):
 						f = e 
 						h = g
-					cp.append([[csp[0][0],csp[0][1]]])
-					g += 1  
-				'''    
-				np1 = []
+					g += 1
+				for csp in p[0]:                   
+					b = csp[0][0]-c
+					e = csp[0][1]-f
+					cp.append([[b+10,e+10]])
+				#............................................................
+				np = []
 				Lfirst = cp[0 : h] 
 				Lsecond = cp[h :]
-				np1 = Lsecond + Lfirst
-				for csp in np1:
-					cmd = 'L'                
+				np = Lsecond + Lfirst    
+				np1 = []
+				
+				for csp in np:
+					
+					cmd = 'L'                     
+					x=str(round(self.uutounit(csp[0][0],"mm" ),2))
+					y=str(round(self.uutounit(csp[0][1],"mm" ),2))
 					if first:
-						x=str(round(self.uutounit(csp[0][0],"mm" ),2))
-						y=str(round(self.uutounit(csp[0][1],"mm" ),2))
+						cmd = 'M'
+						first = False
+						
 						line1 = inkex.etree.SubElement(self.current_layer, inkex.addNS('line','svg'), {'id': 'costy', 'x1': '0', 'y1': '0', 'style': 'stroke-width:1;stroke:red', 'x2': x, 'y2': y} ) 	
+						
 						gcode1 = "G01 X" + x + " Y" + y + "\n"
 						gcode += gcode1						#line1 = inkex.etree.SubElement(self.current_layer, inkex.addNS('line','svg'), {'id': 'costy1','x1': '0', 'y0': '0', 'style': 'stroke-width:1;stroke:red', 'x2': x, 'y2': y} ) 	
-					first = False	
-					gcode += "G01 X" +str(round(self.uutounit(csp[0][0],"mm" ),2)) + " Y" + str(round(self.uutounit(csp[0][1],"mm" ),2)) + "\n"
-				gcode += "G01 X" + x + " Y" + y + "\n"
+						
+					np1.append([cmd,[csp[0][0],csp[0][1]]])	
+					gcode += "G01 X" +x + " Y" + y + "\n"
+				    #gcode += "G01 X" + x + " Y" + y + "\n"
+				node.set('d',simplepath.formatPath(np1))
+				'''
+				inkex.debug(np1)
+				'''
+				gcode += gcode1
 				gcode += "G01 X0 Y0\n"
+				
 				f = open("costycnc.nc", 'w')
 				f.write(str(gcode))
 				f.close()	
-				#if (node.attrib['id']=='costy'):
-					#cPathNode.getparent().remove(cPathNode)
+				
 if __name__ == '__main__':
 	e = MyEffect()
 	e.affect()
